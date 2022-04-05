9d6b8b7ab2800a2f4333bb370873d96356162d88
yash n chavan <yash.n.chavan@apiseroinc.onmicrosoft.com>
2022-04-05 16:17:56 +0530

    Test Commit Demo3

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "demo_db" {
+resource "snowflake_database" "demo_db_sec" {
   name    = "DEMO_DB_sec"
   comment = "Database for Snowflake Terraform demo"
 }
 
 resource "snowflake_schema" "demo_schema" {
-  database = snowflake_database.demo_db.name
+  database = snowflake_database.demo_db_sec.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
